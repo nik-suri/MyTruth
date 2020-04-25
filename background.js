657e9dfa81d32a85ce72a81797a81476295c6ac2
Nikhil Suri <nikhilsuri@comcast.net>
2020-04-24 23:29:01 -0700

    pure storage functionality with minimal css

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('installed')
+
+  // instantiate values
+  chrome.storage.sync.set({
+    trueBeliefs: [],
+    falseBeliefs: []
+  }, function() {
+    console.log('initial belief values (empty) set.')
+  })
 })
--- popup.js
+++ popup.js
@@ -10,9 +10,39 @@ window.onload = function () {
 }
 
 document.getElementById('trueBtn').onclick = function() {
-  bkg.console.log('true')
+  const belief = document.getElementById('selection').textContent
+
+  chrome.storage.sync.get('trueBeliefs', function(data) {
+    const currentValue = data.trueBeliefs
+    bkg.console.log('Value currently is ', currentValue)
+
+    const newValue = currentValue.concat(belief)
+
+    chrome.storage.sync.set({ trueBeliefs: newValue}, function() {
+      bkg.console.log('Value is set to ', newValue);
+    })
+  })
 }
 
 document.getElementById('falseBtn').onclick = function() {
-  bkg.console.log('false')
+  const belief = document.getElementById('selection').textContent
+
+  chrome.storage.sync.get('falseBeliefs', function(data) {
+    const currentValue = data.falseBeliefs
+    bkg.console.log('Value currently is ', currentValue)
+    
+    const newValue = currentValue.concat(belief)
+
+    chrome.storage.sync.set({ falseBeliefs: newValue}, function() {
+      bkg.console.log('Value is set to ', newValue);
+    })
+  })
+}
+
+document.getElementById('seeMoreBtn').onclick = function() {
+  chrome.storage.sync.get(['trueBeliefs', 'falseBeliefs'], function(data) {
+    bkg.console.log(data)
+    bkg.console.log('True beliefs', data.trueBeliefs)
+    bkg.console.log('False beliefs', data.falseBeliefs)
+  })
 }
